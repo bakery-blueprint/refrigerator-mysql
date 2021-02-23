7f443d993243f77226a2b68fdcf26fd90cef6d6f
hotire <gngh0101@gmail.com>
2021-02-24 00:41:23 +0900

    init

---
+++ src/test/kotlin/com/github/blueprint/refrigerator/mysql/RefrigeratorMysqlApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.github.blueprint.refrigerator.mysql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RefrigeratorMysqlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
