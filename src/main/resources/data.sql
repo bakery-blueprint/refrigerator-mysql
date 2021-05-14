d2fefc0355a6c7d21eb1e261447f27565dc2de7a
hotire <gngh0101@gmail.com>
2021-05-15 00:14:38 +0900

    Add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Products (prod_name, prod_price) values ('베베숲 물티슈', 2700);
+INSERT INTO Products (prod_name, prod_price) values ('여름 토퍼', 35180);
+INSERT INTO Products (prod_name, prod_price) values ('페이크 삭스', 860);
+INSERT INTO Products (prod_name, prod_price) values ('우산', 2900);
